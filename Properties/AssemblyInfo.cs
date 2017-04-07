bd3315435c8b673bff3c8461787267c4ea060e58
Andrew Benton <andrew.j.benton@avanade.com>
2017-04-07 09:30:06 -0700

    Remove all VS & NuGet artifacts

--- Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Allow Clipboard")]
-[assembly: AssemblyDescription("Chrome extension to allow clipboard read and write without user interaction")]
-[assembly: AssemblyProduct("Allow Clipboard")]
-[assembly: AssemblyVersion("1.0.*")]
